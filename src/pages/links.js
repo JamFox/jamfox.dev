c0a98aa125e0cdb7facbd1c5c5542abf2ace407f
JamFox <karl-andreas7@hotmail.com>
2022-07-31 11:31:58 +0200

    over 9000 moar links

--- src/pages/links.js
+++ src/pages/links.js
@@ -174,13 +174,27 @@ export default function AboutPage() {
             </Link>
           </TextDate>
           <TextBodySmall>Geocities internet archeology.</TextBodySmall>
-
           <TextDate>
             <Link to="https://neal.fun/absurd-trolley-problems/">
               <LinkText>Absurd Trolley Problems</LinkText>
             </Link>
           </TextDate>
-          <TextBodySmall>Get to know yourself by answering increasingly absurd trolley problems.</TextBodySmall>
+          <TextBodySmall>
+            Get to know yourself by answering increasingly absurd trolley
+            problems.
+          </TextBodySmall>
+          <TextDate>
+            <Link to="https://www.cameronsworld.net/">
+              <LinkText>Cameron's World</LinkText>
+            </Link>
+          </TextDate>
+          <TextBodySmall>A love letter to the Internet of old.</TextBodySmall>
+          <TextDate>
+            <Link to="https://www.youtube.com/playlist?list=PLfINogWdDN60ooq4cglyFNzya34TkJaJQ">
+              <LinkText>Ancient Memes And Internet Classics Playlist</LinkText>
+            </Link>
+          </TextDate>
+          <TextBodySmall>Trip down to 2010.</TextBodySmall>
         </Post>
 
         <Post>
@@ -303,7 +317,6 @@ export default function AboutPage() {
 
         <Post>
           <HeadingL2>Generally Interesting</HeadingL2>
-
           <TextDate>
             <Link to="https://waitbutwhy.com/">
               <LinkText>Wait But Why</LinkText>
@@ -312,34 +325,49 @@ export default function AboutPage() {
           <TextBodySmall>
             The most useful procrastination website you'll ever find.
           </TextBodySmall>
-
           <TextDate>
             <Link to="https://wtfhappenedin1971.com/">
               <LinkText>WTF Happened In 1971?!</LinkText>
             </Link>
           </TextDate>
           <TextBodySmall>Feel the pessimism boiling.</TextBodySmall>
-
           <TextDate>
             <Link to="https://fauux.neocities.org/">
               <LinkText>Fauux Neocity</LinkText>
             </Link>
           </TextDate>
           <TextBodySmall>Wired sound for wired people.</TextBodySmall>
-
           <TextDate>
             <Link to="http://hasthelargehadroncolliderdestroyedtheworldyet.com/">
               <LinkText>Has The LHC Destroyed The World Yet?</LinkText>
             </Link>
           </TextDate>
           <TextBodySmall>Stay in touch, just to make sure.</TextBodySmall>
-
           <TextDate>
             <Link to="https://www.youtube.com/c/3blue1brown">
               <LinkText>3Blue1Brown</LinkText>
             </Link>
           </TextDate>
           <TextBodySmall>The best math channel, in the universe.</TextBodySmall>
+          <TextDate>
+            <Link to="https://shoulditbeameeting.com/">
+              <LinkText>Should it be a meeting?</LinkText>
+            </Link>
+          </TextDate>
+          <TextBodySmall>
+            Please ask yourself this question every time.
+          </TextBodySmall>
+          <TextDate>
+            <Link to="https://old.reddit.com/r/AskReddit/comments/24vzgl/you_just_won_a_656_million_dollar_lottery_what_do/chba4bf">
+              <LinkText>
+                You just won a 656 Million Dollar Lottery. What do you do now?
+              </LinkText>
+            </Link>
+          </TextDate>
+          <TextBodySmall>
+            /u/BlakeClass's classic, thought provoking advice on what to do
+            after a lottery win.
+          </TextBodySmall>
         </Post>
 
         <Post>
@@ -389,6 +417,39 @@ export default function AboutPage() {
           <TextBodySmall>
             The unfathomable stories, wars, exploits and hacks from the oldest
             anarchy server in minecraft.
+          </TextBodySmall>
+        </Post>
+
+        <Post>
+          <HeadingL2>Essays</HeadingL2>
+          <TextDate>
+            <Link to="https://www.currentaffairs.org/2018/03/the-intellectual-we-deserve">
+              <LinkText>The Intellectual We Deserve</LinkText>
+            </Link>
+          </TextDate>
+          <TextBodySmall>
+            Jordan Peterson's popularity is the sign of a deeply impoverished
+            political and intellectual landscape…
+          </TextBodySmall>
+          <TextDate>
+            <Link to="https://youtu.be/MEX-f_c9w0A">
+              <LinkText>
+                "Turn Ur Brain Off" - The Purpose and Value of Media Analysis
+              </LinkText>
+            </Link>
+          </TextDate>
+          <TextBodySmall>
+            Counterargument to the all too tiring "just to turn your brain off."
+          </TextBodySmall>
+          <TextDate>
+            <Link to="https://www.polygon.com/2018/12/17/18142124/shitposting-memes-dada-art-history">
+              <LinkText>
+                Shitposting is an art, if history is any indication
+              </LinkText>
+            </Link>
+          </TextDate>
+          <TextBodySmall>
+            Drawing a line between an internet pastime and the Dadaist movement.
           </TextBodySmall>
         </Post>
 
